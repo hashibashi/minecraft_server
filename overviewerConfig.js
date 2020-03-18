a197020322c60e9a6e5cd741e3ec4dd95c90a269
unknown <hashibashi>
2020-03-17 22:47:42 -0400

    Init Commit

---
+++ overviewerConfig.js
@@ -0,0 +1,67 @@
+var overviewerConfig = {
+    "CONST": {
+        "tileSize": 384,
+        "image": {
+            "defaultMarker": "signpost.png",
+            "signMarker": "signpost_icon.png",
+            "bedMarker": "bed.png",
+            "spawnMarker": "icons/marker_home.png",
+            "spawnMarker2x": "icons/marker_home_2x.png",
+            "queryMarker": "icons/marker_location.png",
+            "queryMarker2x": "icons/marker_location_2x.png",
+            "compass": {
+                "0": "compass_upper-left.png",
+                "1": "compass_upper-right.png",
+                "3": "compass_lower-left.png",
+                "2": "compass_lower-right.png"
+            }
+        },
+        "mapDivId": "mcmap",
+        "UPPERLEFT": 0,
+        "UPPERRIGHT": 1,
+        "LOWERLEFT": 3,
+        "LOWERRIGHT": 2
+    },
+    "worlds": [
+        "Server World - overworld"
+    ],
+    "map": {
+        "debug": true,
+        "cacheTag": "1584499448",
+        "north_direction": "lower-left",
+        "controls": {
+            "pan": true,
+            "zoom": true,
+            "spawn": true,
+            "compass": true,
+            "mapType": true,
+            "overlays": true,
+            "coordsBox": true
+        }
+    },
+    "tilesets": [
+        {
+            "name": "Server World",
+            "zoomLevels": 9,
+            "defaultZoom": 6,
+            "maxZoom": 9,
+            "path": "day",
+            "base": "",
+            "bgcolor": "#1a1a1a",
+            "world": "Server World - overworld",
+            "last_rendertime": 1584499267,
+            "imgextension": "png",
+            "isOverlay": false,
+            "poititle": "Markers",
+            "showlocationmarker": true,
+            "center": [
+                3087,
+                75,
+                1302
+            ],
+            "minZoom": 0,
+            "spawn": false,
+            "north_direction": 0
+        }
+    ]
+};
